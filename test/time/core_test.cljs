24238627c00767c175a221c246976bbbab476196
Isaac Zeng <ndtm.idea@gmail.com>
2017-07-17 16:09:34 +0800

    add benchmark for parse

--- test/time/core_test.cljs
+++ test/time/core_test.cljs
@@ -33,3 +33,28 @@
 (time
  (dotimes [_ 10000]
    (.format (m) "YYYY[年]MM[月]DD[日] HH[时]mm[分]ss[秒]")))
+
+
+(println "~~~~~~~~~~~~~~~~ parse benchmark ~~~~~~~~~~~~~~~~")
+(println
+ (let [s (.format (m) "YYYY[年]MM[月]DD[日] HH[时]mm[分]ss[秒]")]
+   (m s "YYYY[年]MM[月]DD[日] HH[时]mm[分]ss[秒]")))
+(time
+ (let [s (.format (m) "YYYY[年]MM[月]DD[日] HH[时]mm[分]ss[秒]")]
+   (dotimes [_ 10000]
+     (m s "YYYY[年]MM[月]DD[日] HH[时]mm[分]ss[秒]"))))
+(time
+ (dotimes [_ 10000]
+   (m "2017-07-17T16:29:33")))
+
+(println
+ (let [s (t/format (t/date) "yyyy'年'MM'月'dd'日' HH'时'mm'分'ss'秒'")]
+   (t/parse s "yyyy'年'MM'月'dd'日' HH'时'mm'分'ss'秒'")))
+(time
+ (let [s (t/format (t/date) "yyyy'年'MM'月'dd'日' HH'时'mm'分'ss'秒'")]
+   (dotimes [i 10000]
+     (t/parse s "yyyy'年'MM'月'dd'日' HH'时'mm'分'ss'秒'"))))
+(time
+ (let [s (t/format (t/date) "yyyy-MM-dd'T'HH-mm-ss")]
+   (dotimes [i 10000]
+     (t/parse s "yyyy-MM-dd'T'HH-mm-ss"))))
